dba600cf5c890777b1fbb9b74a87b1e86fdc9997
Ayush-Thakur2005 <ayushintern05@gmail.com>
2025-02-14 20:18:21 +0530

    Backend server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+const TaskSchema = new mongoose.Schema({ text: String });
+const Task = mongoose.model("Task", TaskSchema);
+
+app.get("/api/tasks", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.post("/api/tasks", async (req, res) => {
+  const task = new Task({ text: req.body.text });
+  await task.save();
+  res.json(task);
+});
+
+app.delete("/api/tasks/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.sendStatus(204);
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
